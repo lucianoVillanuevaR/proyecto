4471dfcb5cfca6b875ad48665d6d6204b255966f
Luciano Villanueva Ramirez <lucianoivillanuevar@gmail.com>
2025-07-25 22:02:07 -0400

    Delete src directory

--- src/components/LoanMessage.jsx
+++
@@ -1,9 +0,0 @@
-// src/components/LoanMessage.jsx
-export default function LoanMessage({ mensaje, error }) {
-  return (
-    <div className="mt-4">
-      {mensaje && <p className="text-green-600 font-semibold">{mensaje}</p>}
-      {error && <p className="text-red-600 font-semibold">{error}</p>}
-    </div>
-  );
-}
